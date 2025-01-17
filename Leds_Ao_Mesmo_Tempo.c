87e86e29591b46ebadb2d01d5250cbdc9ed50e99
F3lipe09 <felipesantana.carmo09@gmail.com>
2025-01-17 16:51:03 -0300

    Criação função blink dos leds

--- Leds_Ao_Mesmo_Tempo.c
+++ Leds_Ao_Mesmo_Tempo.c
@@ -5,6 +5,8 @@
 #define Led_Blue_Pin 12
 #define Led_Red_Pin 13
 
+const TimeOn = 500;
+const TimeOff = 500;
 
 // Função de setup dos leds
 void initLeds (){
@@ -17,4 +19,19 @@ void initLeds (){
     gpio_set_dir(Led_Blue_Pin, GPIO_OUT);
     gpio_set_dir(Led_Red_Pin, GPIO_OUT);
 
+}
+
+void BlinkLeds (){
+
+    gpio_put(Led_Green_Pin, 1);
+    gpio_put(Led_Blue_Pin, 1);
+    gpio_put(Led_Red_Pin, 1);
+    printf("\n Leds ON.");
+    sleep_ms(TimeOn);
+
+    gpio_put(Led_Green_Pin, 0);
+    gpio_put(Led_Blue_Pin, 0);
+    gpio_put(Led_Red_Pin, 0);
+    printf("\n Leds OFF.");
+    sleep_ms(TimeOff);
 }
