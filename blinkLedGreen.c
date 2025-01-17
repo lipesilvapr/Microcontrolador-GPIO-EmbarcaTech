cd7b9692f918d5b82a08350d9931a64297028778
Alisson Livio <livilia74629@gmail.com>
2025-01-17 14:01:26 -0300

    feat: inicialização do LED

--- blinkLedGreen.c
+++ blinkLedGreen.c
@@ -1,6 +1,19 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+
+#define LED_GREEN_PIN 11
+
+//Função para configuração do LED
+void initLed(){
+    gpio_init(LED_GREEN_PIN);
+    gpio_set_dir(LED_GREEN_PIN, GPIO_OUT);
+    gpio_put(LED_GREEN_PIN, 0);
+
+}
+
 int main(){
+
+    initLed();
     
 }
